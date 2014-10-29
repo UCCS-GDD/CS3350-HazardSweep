74ab84bd7934cd95e30eeab9a8f81bf181bab4d5
tkrejci <tikrejci@gmail.com>
2014-10-28 20:18:27 -0600

    added room framework

--- Hazard Sweep/Hazard Sweep/Hazard_Sweep/Classes/Room.cs
+++ Hazard Sweep/Hazard Sweep/Hazard_Sweep/Classes/Room.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Hazard_Sweep.Classes
 {
-    class Room
+    class Room : Sprite
     {
+        private Rectangle boundary;
+        private int row;
+        private int column;
+        private bool up;
+        private bool down;
+
+        public Room(Game game, string textureFile, Vector2 position, Rectangle boundary, int row, int column, bool up, bool down)
+            : base(game, textureFile, position)
+        {
+            this.boundary = boundary;
+            this.row = row;
+            this.column = column;
+            this.up = up;
+            this.down = down;
+        }
+
     }
 }
