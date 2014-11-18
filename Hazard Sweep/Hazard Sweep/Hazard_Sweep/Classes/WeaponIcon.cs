929f73c49ee8239eaeda973518407a288dd4af8f
nkasza <nkasza@uccs.edu>
2014-11-18 16:37:24 -0700

    FIXED AGAIN

--- Hazard Sweep/Hazard Sweep/Hazard_Sweep/Classes/WeaponIcon.cs
+++ Hazard Sweep/Hazard Sweep/Hazard_Sweep/Classes/WeaponIcon.cs
@@ -32,7 +32,15 @@ namespace Hazard_Sweep.Classes
         {
             drawRec = new Rectangle(x, 0, 64, 64);
             sRec = new Rectangle(0, 0, 128, 128);
+        }
+        
+        public void LoadContent(Game game)
+        {
+            texture = game.Content.Load<Texture2D>(textureFile);  
+        }
 
+        public void Update(GameTime gameTime)
+        {
             if (player.hasPistol == true && type == WeaponType.Pistol)
             {
                 WeaponAcquired();
@@ -50,61 +58,6 @@ namespace Hazard_Sweep.Classes
             {
                 WeaponEquipped();
             }
-
-        }
-        
-        public void LoadContent(Game game)
-        {
-            texture = game.Content.Load<Texture2D>(textureFile);  
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            KeyboardState ks = Keyboard.GetState();
-
-
-            if (ks.IsKeyDown(Keys.D2))
-            {
-                if (player.hasPistol == true)
-                {
-                    if (type == WeaponType.Pistol)
-                    {
-                        WeaponEquipped();
-                    }
-                    else
-                    {
-                        WeaponAcquired();
-                    }
-                }
-            }
-            if (ks.IsKeyDown(Keys.D3))
-            {
-                if (player.hasAssaultRifle == true)
-                {
-                    if (type == WeaponType.AssaultRifle)
-                    {
-                        WeaponEquipped();
-                    }
-                    else
-                    {
-                        WeaponAcquired();
-                    }
-                }
-            }
-            if (ks.IsKeyDown(Keys.D4))
-            {
-                if (player.hasShotgun == true)
-                {
-                    if (type == WeaponType.Shotgun)
-                    {
-                        WeaponEquipped();
-                    }
-                    else
-                    {
-                        WeaponAcquired();
-                    }
-                }
-            }
         }
 
         public void Draw(SpriteBatch sb)
@@ -116,7 +69,7 @@ namespace Hazard_Sweep.Classes
         // weapon acquired
         public void WeaponAcquired()
         {
-            sRec.X = 0;
+            sRec.X = 128;
         }
 
         // weapon equipped
